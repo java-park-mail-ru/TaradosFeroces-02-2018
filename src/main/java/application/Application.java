4000c8d393a01f29cf872e65a3e76ed4d5d3fec6
Alexander Kuznetsov <ad.kuznetsov.a7@Yandex.ru>
2018-02-25 20:01:27 +0300

    chore(all): start spring-boot application <body>

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,18 @@
+package application;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("I am working!");
+        SpringApplication.run(Application.class, args);
+    }
+
+}
