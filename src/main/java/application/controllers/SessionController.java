6f1c2e05c6d55a405d3abcdb9433867c81e96a45
Alexander Kuznetsov <ad.kuznetsov.a7@Yandex.ru>
2018-02-26 19:43:10 +0300

    fix(controllers) : Get -> Post

--- src/main/java/application/controllers/SessionController.java
+++ src/main/java/application/controllers/SessionController.java
@@ -5,7 +5,7 @@ import application.utils.requests.UserSignInRequest;
 import application.utils.responses.Message;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +20,7 @@ public class SessionController {
     private static final String JSON = MediaType.APPLICATION_JSON_UTF8_VALUE; // "application/json;charset=UTF-8"
 
 
-    @GetMapping(path = "/signup", consumes = JSON, produces = JSON)
+    @PostMapping(path = "/signup", consumes = JSON, produces = JSON)
     public Message signup(@RequestBody UserSignInRequest body, HttpSession httpSession) {
         return new Message("Created user " + body.getLogin());
     }
