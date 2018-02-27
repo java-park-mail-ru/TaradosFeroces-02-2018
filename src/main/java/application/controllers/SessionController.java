f0266338dd1f6ffb9497a8cc7881a39c5b0a22cf
Alexander Kuznetsov <ad.kuznetsov.a7@Yandex.ru>
2018-02-27 18:14:50 +0300

    feature(all) : CROS

--- src/main/java/application/controllers/SessionController.java
+++ src/main/java/application/controllers/SessionController.java
@@ -5,20 +5,16 @@ import application.utils.requests.UserSignInRequest;
 import application.utils.responses.Message;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpSession;
 
 
-
-
 @RestController
+@CrossOrigin
 @RequestMapping("/alexalone")
 public class SessionController {
     private static final String JSON = MediaType.APPLICATION_JSON_UTF8_VALUE; // "application/json;charset=UTF-8"
-
 
     @PostMapping(path = "/signup", consumes = JSON, produces = JSON)
     public Message signup(@RequestBody UserSignInRequest body, HttpSession httpSession) {
