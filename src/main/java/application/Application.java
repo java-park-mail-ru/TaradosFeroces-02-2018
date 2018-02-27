c3a24dc6830f05de2c24c360207a5a232b55397f
Alexander Kuznetsov <ad.kuznetsov.a7@Yandex.ru>
2018-02-28 00:07:55 +0300

    chore(all)

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("I am fckn working!");
         SpringApplication.run(Application.class, args);
     }
 }
+
